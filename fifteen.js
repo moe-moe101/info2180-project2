11f9af432b35c6dca2fb19b17d9b57df670074bb
Moesha Amos <moemusiclover7@gmail.com>
2018-11-07 17:49:48 -0800

    added functionality to move selected puzzle piece to neighnouring empty square and applied the hover functionality

--- fifteen.js
+++ fifteen.js
@@ -3,32 +3,87 @@
 //620099794
 
 var pieces=[];
+var emptyX=300;
+var emptyY=300;
 
 window.onload = function(){
 generatePuzzle();
 }
 
-//To each div the puzzlearea applies the puzzlepiece class and positions the divs correctly
+//To each div the puzzlearea applies the puzzlepiece class and positions the divs
 function generatePuzzle(){
-	var x = 0;
-	var y = 3;
+	var j = 0;
+	var m = 3;
 	pieces = $$('#puzzlearea div');
 	
-	for(var k=0; k<pieces.length; k++){
-		for(var l=0; l <= y; l++){
-			pieces[k].addClassName("puzzlepiece");
-			pieces[k].style.top = 100*x + "px";
+	for(var k=0; k < pieces.length; k++){
+		for(var y=0; y <= m; y++){
+			pieces[k].classList.add("puzzlepiece");
+			pieces[k].style.top = 100* j + "px";
 			pieces[k].style.left = 100 * y + "px";
-			pieces[k].style.backgroundPosition = -l * 100 +"px" + x * -100 + "px";
+			pieces[k].style.backgroundPosition = -y * 100 + "px" + j * -100 + "px";
+			pieces[k].addEventListener("click",movePiece);
+			pieces[k].addEventListener("mouseover",hover);
 			k++;
 			
 		}
-		x++;
-		if(x >2){
-			y=2;
+		j++;
+		if(j >2){
+			m=2;
 		}
 		k--;
 	}
 	
 }
 
+function movePiece(event){
+	moveHelp(this);
+}
+
+//applies the css class moveablepiece to the mouse hovers over a piece where any of it's neighbours is a blank square
+function hover(event){
+	if(isNbourEmpty(this.style.left,this.style.top)){
+		this.classList.add("movablepiece");
+	}else if(this.hasClassName("movablepiece")){
+		this.classList.remove("movablepiece")
+	}
+}
+//moves the selected tile into the blank squares position
+function moveHelp(piece){
+	if(isNbourEmpty(piece.style.left,piece.style.top)){
+		var xtemp = piece.style.left;
+		var ytemp = piece.style.top;
+		piece.style.left = emptyX + "px";
+		piece.style.top = emptyY + "px";
+		emptyY = parseInt(ytemp);
+		emptyX = parseInt(xtemp);
+	}
+} 
+
+//checks whether any neighbouring square of the selected tile is empty
+function isNbourEmpty(x,y){
+	if(Math.abs(emptyX - parseInt(x)) == 100){
+		if(Math.abs(emptyY - parseInt(y)) == 0){
+			return true;
+		}
+	}else if(Math.abs(emptyY - parseInt(y)) == 100){
+		if(Math.abs(emptyX - parseInt(x)) == 0){
+			return true;
+		}
+	}
+	return false;
+}
+
+//shuffles the puzzles using 100 random moves
+/*function shuffleBoard(){
+	var tmp =[];
+	for(var j =0;j <100; j++){
+		for(var k= 0; k<pieces.length;k++){
+			if (isNbourEmpty(pieces[k].style.left,pieces[k].style.top)){
+				tmp.push(pieces[k]);
+			}
+		}
+		moveHelp(tmp[Math.floor(Math.random()*tmp.length)]);
+		tmp=[];
+	}
+}*/
